5b84fdfccceda8e0f75ed9091555502e2d28d740
Hilman Yusoh <118374893+hilmanyusoh@users.noreply.github.com>
2024-03-05 16:14:14 +0700

    edit1

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
 from myapp import views
-from . import views
 
 urlpatterns = [
-    path('', views.simulate_bakery_queue, name='simulate_bakery_queue'),
+   path('',views.index),
+    path('about',views.about),
+    path('form',views.form)
 ]
-
-#urlpatterns = [
-#   path('',views.index),
-#    path('about',views.about),
-#    path('form',views.form)
-#]
 
 
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .queue import Queue, bakery_queue
 
 
-# def simulate_bakery_queue(request):
-#    bakery_queue()
-#   return render(request, 'myapp/queue.html')
 
-def simulate_bakery_queue(request):
-    if request.method == 'POST':
-        bakery_output = bakery_queue()
-        return render(request, 'myapp/simulation_result.html', {'bakery_output': bakery_output})
-    else:
-        return render(request, 'bakery_queue/simulation_form.html')
+def index(reques):
+    return render(reques,"index.html")
 
+def about(reques):
+   return render(reques,"about.html")
 
-# Create your views here.
-# def index(reques):
-#    return render(reques,"index.html")
-
-# def about(reques):
-#   return render(reques,"about.html")
-
-# def form(reques):
-#   return render(reques,"form.html")
+def form(reques):
+   return render(reques,"form.html")
