8893a365a1286d3a40aebd21e93158297359ab6e
Hilman Yusoh <118374893+hilmanyusoh@users.noreply.github.com>
2024-03-07 14:00:23 +0700

    web

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,8 +3,6 @@ from myapp import views
 
 urlpatterns = [
    path('',views.index),
-    path('about',views.about),
-    path('form',views.form)
 ]
 
 
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,8 +6,3 @@ from django.http import HttpResponse
 def index(reques):
     return render(reques,"index.html")
 
-def about(reques):
-   return render(reques,"about.html")
-
-def form(reques):
-   return render(reques,"form.html")
