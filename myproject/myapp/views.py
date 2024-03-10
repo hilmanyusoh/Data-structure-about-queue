fb7437ff3b53e0e3722213fb7f404dbc6f6d3434
Hilman Yusoh <118374893+hilmanyusoh@users.noreply.github.com>
2024-03-10 23:18:15 +0700

    web

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import JsonResponse
 from .models import Queue
 
 
 def index(request):
     return render(request,"index.html")
 
-def add_person_to_queue(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        order = request.POST.get('order')
-        if name and order:
-            new_person = Queue.objects.create(name=name, order=order)
-            return JsonResponse({'message': 'Person added to queue successfully'})
-        else:
-            return JsonResponse({'error': 'Please provide both name and order'}, status=400)
-    else:
-        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
