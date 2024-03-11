3dafdf1edcd6f18079283d2167f8066c2543f208
Hilman Yusoh <118374893+hilmanyusoh@users.noreply.github.com>
2024-03-11 11:03:38 +0700

    web

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Queue
 
+"""
+def index(request):
+    return render(request,"index.html")
+"""
 
 def index(request):
     return render(request,"index.html")
